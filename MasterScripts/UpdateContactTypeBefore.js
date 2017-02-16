7db19aa4a3d9ff4f14e332881d8fecac19d7d13f
David Bischof <dbischof@accela.com>
2017-02-16 18:05:39 -0500

    Commit New include logic

--- MasterScripts/UpdateContactTypeBefore.js
+++ MasterScripts/UpdateContactTypeBefore.js
@@ -1,5 +1,5 @@
 /*------------------------------------------------------------------------------------------------------/
-| Program : UpdateContactTypeBeforeV3.0.js
+| Program : UpdateContactTypeBeforeV.js
 | Event   : UpdateContactTypeBefore
 |
 | Usage   : Designed to work with most events and generate a generic framework to expose standard master scirpt functionality
@@ -36,7 +36,7 @@ if (triggerEvent != ""){
 /*------------------------------------------------------------------------------------------------------/
 | END User Configurable Parameters
 /------------------------------------------------------------------------------------------------------*/
-var SCRIPT_VERSION = 3.0;
+var SCRIPT_VERSION = 9.0;
 
 var useSA = false;
 var SA = null;
@@ -49,47 +49,51 @@ if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
 	if (bzr.getSuccess()) { SAScript = bzr.getOutput().getDescription(); }
 	}
 	
+var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";
+var doStdChoices = true; // compatibility default
+var doScripts = false;
+var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice).getOutput().size() > 0;
+if (bzr) {
+	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "STD_CHOICE");
+	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
+	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "SCRIPT");
+	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
+	var bvr3 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "USE_MASTER_INCLUDES");
+	if (bvr3.getSuccess()) {if(bvr3.getOutput().getDescription() == "No") useCustomScriptFile = false}; 
+}
+
 if (SA) {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA));
-	eval(getScriptText(SAScript,SA));
-	}
-else {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS"));
-	}
-	
-eval(getScriptText("INCLUDES_CUSTOM"));
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA,useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA,useCustomScriptFile));
+	eval(getScriptText(SAScript, SA));
+} else {
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useCustomScriptFile));
+}
+
+eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));
 
 if (documentOnly) {
-	doStandardChoiceActions(controlString,false,0);
+	doStandardChoiceActions(controlString, false, 0);
 	aa.env.setValue("ScriptReturnCode", "0");
 	aa.env.setValue("ScriptReturnMessage", "Documentation Successful.  No actions executed.");
 	aa.abortScript();
-	}
-
-var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX",vEventName);
-
-var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";
-var doStdChoices = true;  // compatibility default
-var doScripts = false;
-var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice ).getOutput().size() > 0;
-if (bzr) {
-	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"STD_CHOICE");
-	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
-	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice ,"SCRIPT");
-	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
 }
 
-function getScriptText(vScriptName){
-	var servProvCode = aa.getServiceProviderCode();
-	if (arguments.length > 1) servProvCode = arguments[1]; // use different serv prov code
-	vScriptName = vScriptName.toUpperCase();	
+var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX", vEventName);
+
+function getScriptText(vScriptName, servProvCode, useProductScripts) {
+	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();
+	vScriptName = vScriptName.toUpperCase();
 	var emseBiz = aa.proxyInvoker.newInstance("com.accela.aa.emse.emse.EMSEBusiness").getOutput();
 	try {
-		var emseScript = emseBiz.getScriptByPK(servProvCode,vScriptName,"ADMIN");
-		return emseScript.getScriptText() + "";	
-		} catch(err) {
+		if (useProductScripts) {
+			var emseScript = emseBiz.getMasterScript(aa.getServiceProviderCode(), vScriptName);
+		} else {
+			var emseScript = emseBiz.getScriptByPK(aa.getServiceProviderCode(), vScriptName, "ADMIN");
+		}
+		return emseScript.getScriptText() + "";
+	} catch (err) {
 		return "";
 	}
 }
--- MasterScripts/WorkflowTaskUpdateAfter.js
+++ MasterScripts/WorkflowTaskUpdateAfter.js
@@ -1,6 +1,6 @@
 /*------------------------------------------------------------------------------------------------------/
 | SVN $Id: WorkflowTaskUpdateAfter.js 6515 2012-03-16 18:15:38Z john.schomp $
-| Program : WorkflowTaskUpdateAfterV2.0.js
+| Program : WorkflowTaskUpdateAfter.js
 | Event   : WorkflowTaskUpdateAfter
 |
 | Usage   : Master Script by Accela.  See accompanying documentation and release notes.
@@ -26,8 +26,8 @@ var documentOnly = false;						// Document Only -- displays hierarchy of std cho
 /*------------------------------------------------------------------------------------------------------/
 | END User Configurable Parameters
 /------------------------------------------------------------------------------------------------------*/
-var SCRIPT_VERSION = 3.0;
-var useCustomScriptFile = true;  // if true, use Events->Custom Script, else use Events->Scripts->INCLUDES_CUSTOM
+var SCRIPT_VERSION = 9.0;
+var useCustomScriptFile = true;  // if true, use Events->Custom Script and Master Scripts, else use Events->Scripts->INCLUDES_*
 var useSA = false;
 var SA = null;
 var SAScript = null;
@@ -41,13 +41,26 @@ if (bzr.getSuccess() && bzr.getOutput().getAuditStatus() != "I") {
 	}
 }
 
+var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";
+var doStdChoices = true; // compatibility default
+var doScripts = false;
+var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice).getOutput().size() > 0;
+if (bzr) {
+	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "STD_CHOICE");
+	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
+	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "SCRIPT");
+	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
+	var bvr3 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "USE_MASTER_INCLUDES");
+	if (bvr3.getSuccess()) {if(bvr3.getOutput().getDescription() == "No") useCustomScriptFile = false}; 
+}
+
 if (SA) {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA));
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS", SA,useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS", SA,useCustomScriptFile));
 	eval(getScriptText(SAScript, SA));
 } else {
-	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS"));
-	eval(getScriptText("INCLUDES_ACCELA_GLOBALS"));
+	eval(getScriptText("INCLUDES_ACCELA_FUNCTIONS",null,useCustomScriptFile));
+	eval(getScriptText("INCLUDES_ACCELA_GLOBALS",null,useCustomScriptFile));
 }
 
 eval(getScriptText("INCLUDES_CUSTOM",null,useCustomScriptFile));
@@ -60,17 +73,6 @@ if (documentOnly) {
 }
 
 var prefix = lookup("EMSE_VARIABLE_BRANCH_PREFIX", vEventName);
-
-var controlFlagStdChoice = "EMSE_EXECUTE_OPTIONS";
-var doStdChoices = true; // compatibility default
-var doScripts = false;
-var bzr = aa.bizDomain.getBizDomain(controlFlagStdChoice).getOutput().size() > 0;
-if (bzr) {
-	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "STD_CHOICE");
-	doStdChoices = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
-	var bvr1 = aa.bizDomain.getBizDomainByValue(controlFlagStdChoice, "SCRIPT");
-	doScripts = bvr1.getSuccess() && bvr1.getOutput().getAuditStatus() != "I";
-}
 
 function getScriptText(vScriptName, servProvCode, useProductScripts) {
 	if (!servProvCode)  servProvCode = aa.getServiceProviderCode();
