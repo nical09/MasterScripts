07c75d81d3ba4304c5492b8876bcd7a1a7168f2b
Jason Plaisted <jplaisted@accela.com>
2017-04-27 10:53:43 -0400

    runReportAttach now supports hashtables in addition to parameter value pairs

--- INCLUDES_ACCELA_FUNCTIONS/runReportAttach.js
+++ INCLUDES_ACCELA_FUNCTIONS/runReportAttach.js
@@ -1,9 +1,19 @@
-
+/**
+ * Runs a report with any specified parameters and attaches it to the record
+ *
+ * @example
+ *		runReportAttach(capId,"ReportName","altid",capId.getCustomID(),"months","12");
+ *		runReportAttach(capId,"ReportName",paramHashtable);
+ * @param capId
+ *			itemCapId - capId object 
+ * @param {report parameter pairs} or {hashtable}
+ *			optional parameters are report parameter pairs or a parameters hashtable
+ * @returns {boolean}
+ *			if the report was generated and attached return true
+ *
+ */
 function runReportAttach(itemCapId,aaReportName)
 	{
-	// optional parameters are report parameter pairs
-	// for example: runReportAttach(capId,"ReportName","altid",capId.getCustomID(),"months","12");
-	
 
 	var reportName = aaReportName;
 
@@ -15,32 +25,43 @@ function runReportAttach(itemCapId,aaReportName)
 	var report = reportResult.getOutput(); 
 
 	var itemCap = aa.cap.getCap(itemCapId).getOutput();
-	appTypeResult = itemCap.getCapType();
-	appTypeString = appTypeResult.toString(); 
-	appTypeArray = appTypeString.split("/");
+	itemAppTypeResult = itemCap.getCapType();
+	itemAppTypeString = itemAppTypeResult.toString(); 
+	itemAppTypeArray = itemAppTypeString.split("/");
 
-	report.setModule(appTypeArray[0]); 
+	report.setModule(itemAppTypeArray[0]); 
 	report.setCapId(itemCapId.getID1() + "-" + itemCapId.getID2() + "-" + itemCapId.getID3()); 
 	report.getEDMSEntityIdModel().setAltId(itemCapId.getCustomID());
 
-	var parameters = aa.util.newHashMap();              
+	var parameters = aa.util.newHashMap(); 
 
-	for (var i = 2; i < arguments.length ; i = i+2)
+	if(arguments.length > 2 && arguments[2].getClass().toString().equals("class java.lang.String")){
+		// optional parameters are report parameter pairs
+		// for example: runReportAttach(capId,"ReportName","altid",capId.getCustomID(),"months","12");
+		for (var i = 2; i < arguments.length ; i = i+2)
 		{
-		parameters.put(arguments[i],arguments[i+1]);
-		logDebug("Report parameter: " + arguments[i] + " = " + arguments[i+1]);
-		}	
+			parameters.put(arguments[i],arguments[i+1]);
+			logDebug("Report parameter: " + arguments[i] + " = " + arguments[i+1]);
+		}
+	}
+	else if(arguments.length > 2 && arguments[2].getClass().toString().equals("class java.util.HashMap")){
+		// optional argument is a hashmap so assign it to parameters
+		parameters = arguments[2]
+	}
 
 	report.setReportParameters(parameters);
 
 	var permit = aa.reportManager.hasPermission(reportName,currentUserID); 
 	if(permit.getOutput().booleanValue()) 
 		{ 
-		var reportResult = aa.reportManager.getReportResult(report); 
-
-		logDebug("Report " + aaReportName + " has been run for " + itemCapId.getCustomID());
-
+			var reportResult = aa.reportManager.getReportResult(report); 
+			if(reportResult){
+				logDebug("Report " + aaReportName + " has been run for " + itemCapId.getCustomID());
+				return true;
+			}
 		}
-	else
+	else{
 		logDebug("No permission to report: "+ reportName + " for user: " + currentUserID);
+		return false;
+	}
 }
